d03a5fd85c7f291546b8a51ac206a7c77c999cd0
Pushyan Jhaveri <121259940+PushyanJhaveri@users.noreply.github.com>
2025-03-17 07:41:54 -0400

    Update 01_data_exploration.py

--- notebooks/01_data_exploration.py
+++ notebooks/01_data_exploration.py
@@ -1,1 +1,20 @@
+# Churn distribution
+sns.countplot(x='Churn', data=data)
+plt.title('Churn Distribution')
+plt.show()
 
+# Churn by gender
+sns.countplot(x='gender', hue='Churn', data=data)
+plt.title('Churn by Gender')
+plt.show()
+
+# Churn by contract type
+sns.countplot(x='Contract', hue='Churn', data=data)
+plt.title('Churn by Contract Type')
+plt.show()
+
+# Correlation heatmap
+plt.figure(figsize=(12, 8))
+sns.heatmap(data.corr(), annot=True, fmt='.2f', cmap='coolwarm')
+plt.title('Correlation Heatmap')
+plt.show()
