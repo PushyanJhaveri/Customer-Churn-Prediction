e0330d0f51f155b9763f4da7457f23f5f8d07248
Pushyan Jhaveri <121259940+PushyanJhaveri@users.noreply.github.com>
2025-03-17 07:46:26 -0400

    Create 02_model_development.py

---
+++ notebooks/02_model_development.py
@@ -0,0 +1,11 @@
+# Define features and target variable
+X = data.drop('Churn', axis=1)
+y = data['Churn']
+
+# Split the dataset into training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+# Feature Scaling
+scaler = StandardScaler()
+X_train = scaler.fit_transform(X_train)
+X_test = scaler.transform(X_test)
