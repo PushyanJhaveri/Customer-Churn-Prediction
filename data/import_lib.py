5e76763d985a4302392322a3beef6dddf7d46395
Pushyan Jhaveri <121259940+PushyanJhaveri@users.noreply.github.com>
2025-03-17 08:51:14 -0400

    Create import_lib.py

---
+++ data/import_lib.py
@@ -0,0 +1,13 @@
+import pandas as pd
+import numpy as np
+import joblib
+import matplotlib.pyplot as plt
+import seaborn as sns
+import plotly.express as px
+from sklearn.model_selection import train_test_split, GridSearchCV
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import classification_report, confusion_matrix, roc_auc_score, accuracy_score
+from sklearn.preprocessing import StandardScaler
+
+# Load the dataset
+data = pd.read_csv('Telco-Customer-Churn.csv')
